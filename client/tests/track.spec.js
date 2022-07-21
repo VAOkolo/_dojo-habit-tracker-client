4d70dcee14c7a68027cf4f6c347e3cb3947e83a2
VAOkolo <vincentaokolo@outlook.com>
2022-07-21 15:23:15 +0100

    track testing

---
+++ client/tests/track.spec.js
@@ -0,0 +1,34 @@
+const renderDom = require('./trackHelpers')
+
+let dom;
+let document;
+
+
+describe('index.html', () => {
+    beforeEach( async () => {
+        dom = await renderDom('client/track.html')
+        document = await dom.window.document;
+    })
+
+    test('it has a form for adding habits', () => {
+        let form = document.getElementById('habit-form')
+        // console.log(header)
+        expect(form).toBeTruthy()
+    })
+
+    test('has list of habits that can be deleted', () => {
+        let li = document.getElementById('habit-delete-container')
+        expect(li).toBeTruthy()
+    })
+
+    test('main container for calendar', () => {
+        let container = document.getElementById('container')
+        expect(container).toBeTruthy()
+    })
+
+    test('main container for calendar', () => {
+        let title = document.querySelector('title')
+        expect(title.textContent).toEqual('Calendar App Vanilla JS')
+    })
+
+})
